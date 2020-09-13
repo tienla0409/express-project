df1f30e88bf21116b95d78dfa054e6f02f6c6500
stone0409 <stone04092000@gmail.com>
2020-09-13 08:44:37 +0700

    Improve use body-parser

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // use middler third-party
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 // import Routers
 const loginRouter = require("./models/login");
