448a165b2dbcff4f417b30f849587f24ff293d50
tien0409 <byecodon@gmail.com>
2020-09-15 14:52:28 +0700

    Improve retrieve data follows current page

--- controllers/books/index.js
+++ controllers/books/index.js
@@ -3,22 +3,20 @@ const Book = require("../../models/book.model");
 module.exports = {
   getBooks: async function (req, res, next) {
     try {
-      const listBook = await Book.find();
-
       const page = parseInt(req.query.page) || 1;
       const perPage = 8;
 
       const start = (page - 1) * perPage;
       const end = page * perPage;
 
-      const books = listBook.splice(start, end);
+      const listBook = await Book.find().skip(start).limit(perPage);
 
       const totalPage = Math.ceil(listBook.length / perPage);
       const currentPage = Math.ceil(end / perPage);
 
       res.render("books", {
         title: "List Book",
-        books: books.splice(start, end),
+        listBook,
         currentPage,
         user: req.session.user
       });
