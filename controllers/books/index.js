0551c5f0662599b615475c2a6e2f13cdbb928b6d
tien0409 <byecodon@gmail.com>
2020-09-15 09:05:39 +0700

    Handle logic pagination

--- controllers/books/index.js
+++ controllers/books/index.js
@@ -4,9 +4,18 @@ module.exports = {
   getBooks: async function (req, res, next) {
     try {
       const listBook = await Book.find();
+
+      const page = parseInt(req.query.page) || 1;
+      const perPage = 8;
+
+      const start = (page - 1) * perPage;
+      const end = page * perPage;
+
+      const books = listBook.splice(start, end);
+
       res.render("books", {
         title: "List Book",
-        listBook,
+        books: books.splice(start, end),
         user: req.session.user
       });
     } catch (err) {
