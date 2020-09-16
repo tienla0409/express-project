b7972272aa2658d09b76bc6e93561681ee1881bc
tien0409 <byecodon@gmail.com>
2020-09-16 09:22:55 +0700

    Send email when forgot

---
+++ controllers/forgot/index.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer");
+const generatorPassword = require("generate-password");
+
+module.exports = {
+  getForgot: (req, res, next) => {
+    res.render("forgot", {
+      title: "Forgot Password",
+      user: req.cookies.user
+    });
+  },
+
+  forgotEmail: (req, res, next) => {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USERNAME_EMAIL,
+        pass: process.env.PASSWORD_EMAIL
+      },
+    });
+
+    const mailOptions = {
+      from: "Stone",
+      to: req.body.email,
+      subject: "Reset password",
+      text: generatorPassword.generate({
+        length: 6,
+        numbers: true
+      }),
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) return res.render("forgot", {
+        message: err.message
+      });
+      res.render("forgot", {
+        message: "Check new password in email"
+      });
+    });
+  },
+}
