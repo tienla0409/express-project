ce6373278594e203bcf661e87af0ebdfbcb2cb74
tien0409 <byecodon@gmail.com>
2020-09-17 23:23:45 +0700

    Change authentication login: instead logic in login controller to user model

--- controllers/register/index.js
+++ controllers/register/index.js
@@ -1,5 +1,4 @@
 // import third-party package
-const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 
@@ -13,7 +12,7 @@ const {
 module.exports = {
   getRegister: function (req, res, next) {
     res.render("register.pug", {
-      title: "Register"
+      title: "Register",
     });
   },
 
@@ -27,55 +26,61 @@ module.exports = {
     const messages = [];
 
     const existEmail = await User.findOne({
-      email: email
+      email: email,
     });
 
-    if (!existEmail) { // email not exist => can register
-      if (password === confirmPassword) { // check password match
-        bcrypt.hash(password, 10, (err, hashPassword) => {
-          const userNew = new User({
-            email: email,
-            password: hashPassword,
-          });
+    if (!existEmail) {
+      // email not exist => can register
+      if (password === confirmPassword) {
+        // check password match
+        const userNew = new User({
+          email: email,
+          password: password,
+        });
 
-          userNew.save(errs => {
+        userNew.save((err) => {
+          if (err) return res.status(500).send(err.message);
+
+          const token = new Token({
+            _userId: userNew._id,
+            token: crypto.randomBytes(16).toString("hex"),
+          });
+          token.save((err) => {
             if (err) return res.status(500).send(err.message);
 
-            const token = new Token({
-              _userId: userNew._id,
-              token: crypto.randomBytes(16).toString("hex"),
-            })
-            token.save(err => {
+            const transporter = nodemailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: process.env.USERNAME_EMAIL,
+                pass: process.env.PASSWORD_EMAIL,
+              },
+            });
+
+            const mailOptions = {
+              from: "stone",
+              to: email,
+              subject: "Account verification token",
+              text: "Hello\n\n" +
+                "Please verify your account by clicking the link:\nhttp://" +
+                req.headers.host +
+                "/register/confirmation/" +
+                token.token,
+            };
+
+            transporter.sendMail(mailOptions, (err) => {
               if (err) return res.status(500).send(err.message);
-
-              const transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                  user: process.env.USERNAME_EMAIL,
-                  pass: process.env.PASSWORD_EMAIL
-                },
-              });
-
-              const mailOptions = {
-                from: "stone",
-                to: email,
-                subject: "Account verification token",
-                text: "Hello\n\n" + "Please verify your account by clicking the link:\nhttp:\/\/" + req.headers.host + "\/register\/confirmation\/" + token.token,
-              };
-
-              transporter.sendMail(mailOptions, err => {
-                if (err) return res.status(500).send(err.message);
-                messages.push(`A verification email has been sent to ${email}`)
-              });
+              messages.push(`A verification email has been sent to ${email}`);
             });
           });
         });
         // return res.redirect("/login");
-      } else { // password and confirmPassword not matched
+      } else {
+        // password and confirmPassword not matched
         const message = "Confirm incorrect password";
         messages.push(message);
       }
-    } else { // email exist
+    } else {
+      // email exist
       const message = "Email existed. Please choose another email";
       messages.push(message);
     }
@@ -89,15 +94,22 @@ module.exports = {
     Token.findOne({
       token: req.params.id
     }, (err, token) => {
-      if (!token) return res.status(400).send("We were unable to find a valid token. Your token my have expired");
-      console.log(token._userId)
+      if (!token)
+        return res
+          .status(400)
+          .send(
+            "We were unable to find a valid token. Your token my have expired"
+          );
 
       User.findOne({
-        _id: token._userId,
-        // email: req.body.email
+        _id: token._userId
       }, (err, user) => {
-        if (!user) return res.status(400).send("We were unable to find a user for this token.");
-        if (user.isVerified) return res.status(400).send("This user has already been verified.");
+        if (!user)
+          return res
+            .status(400)
+            .send("We were unable to find a user for this token.");
+        if (user.isVerified)
+          return res.status(400).send("This user has already been verified.");
 
         user.isVerified = true;
         user.save(function (err) {
@@ -105,6 +117,6 @@ module.exports = {
           res.status(200).send("The account has been verified. Please log in.");
         });
       });
-    })
+    });
   },
 };
